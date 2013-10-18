c7d275af4f7dc6fd755267869e0b75257aaed902
Jorge Diaz <jorgedf@sixclicks.net>
2013-10-18 23:44:25 +0200

    Remove dynamic modules.

--- app/api/gaminpad_core.rb
+++ app/api/gaminpad_core.rb
@@ -8,30 +8,14 @@ module GaminpadCore
     format :json
     rescue_from :all, :backtrace => true
     
-    # Loading general modules
-    self.send(:include, GaminpadAuth)
-    self.send(:include, GaminpadGames)
+    # Load modules
+    include GaminpadAuth
+    include GaminpadGames
     
     helpers do
       
-      def load_app_modules(app_slug)
-        modfile_path = File.expand_path("../../apps/#{app_slug}/Modfile",  __FILE__)
-        File.open(modfile_path).each do |line|
-          if line.start_with?('mod')
-            modname = line.match(/'(\S+)'/).captures.first
-            puts "loading module (#{modname})"
-            # Loading custom modules
-            # require "#{GP_MODULES_PATH}/#{modname}/#{modname}"
-            # self.class.send(:include, GaminpadAuth)
-            # APICore.singleton_class.send(:include, GaminpadGames)
-          else
-            puts "Modfile line ignored"
-          end
-        end
-      end
-      
-      
       def valid_app_slug
+        APICore.logger.info "valid_app_slug - self -> #{self.inspect}"
         @app_slug = request.host.split('.').first
         @app = App.find_by_url_slug(@app_slug)
         if @app.nil?
@@ -44,14 +28,13 @@ module GaminpadCore
     end
     
     before do
-      if valid_app_slug
-        load_app_modules(@app_slug)
-      end
+      valid_app_slug
     end
 
     resource :ping do
       desc 'Test purpose. Sends a pong response.'
       get '/' do
+        APICore.logger.info "ping - self -> #{self.inspect}"
         { pong: "true", :app => @app_slug }
       end
     end
