6d41c81cf7c8ef82b8af734d7efd12aa8a173080
Jorge Diaz <jorgedf@sixclicks.net>
2013-10-22 13:35:35 +0200

    Create and delete Games added to GaminpadGames.

--- app/api/gaminpad_games.rb
+++ app/api/gaminpad_games.rb
@@ -14,6 +14,36 @@ module GaminpadGames
           @games = current_player.games
           present @games, with: GaminpadGamesEntities::GameEntity
         end
+        
+        desc 'Create a game'
+        params do
+          optional :title, type: String, regexp: /^[0-9a-zA-Z\s_-]{3,}$/, desc: "Game Title"
+        end
+        post '/' do
+          verify_token
+          @game = Game.new(:player => current_player, :title => params[:title])
+          @game.players << current_player
+          if @game.save
+            present @game, with: GaminpadGamesEntities::GameEntity
+          else
+            error!("Error creating game: #{@game.errors.as_json}", 400)
+          end
+        end
+        
+        desc "Delete a Game"
+        params do
+          requires :id, type: String, desc: "Status ID."
+        end
+        delete ':id' do
+          verify_token
+          @game = current_player.games_owned.find_by_id(params[:id])
+          if @game && @game.destroy
+            { :game => @game.id, :status => :deleted }
+          else
+            error!("Error deleting game with Id(#{params[:id]})", 400)
+          end
+        end
+        
       end
 
     end
